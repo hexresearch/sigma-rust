3a74102eacadf7f3988e486f52c040d271f0d546
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-04-02 17:53:54 +0300

    EASY EASY

--- ergotree-ir/src/lib.rs
+++ ergotree-ir/src/lib.rs
@@ -6,9 +6,9 @@
 #![deny(non_camel_case_types)]
 #![deny(non_snake_case)]
 #![deny(unused_mut)]
-#![deny(dead_code)]
-#![deny(unused_imports)]
-#![deny(missing_docs)]
+// #![deny(dead_code)]
+// #![deny(unused_imports)]
+// #![deny(missing_docs)]
 // Clippy exclusions
 #![allow(clippy::unit_arg)]
 #![deny(broken_intra_doc_links)]
